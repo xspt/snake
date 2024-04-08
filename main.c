39544886a6643ae31814bf086a3766e7aad6468b
xspt <82736724+xspt@users.noreply.github.com>
2024-04-08 13:31:04 -0300

    Refactor

--- main.c
+++ main.c
@@ -8,19 +8,22 @@
 #include <allegro5/allegro_primitives.h>
 #include "snake.h"
 
+// Window size
 #define WIN_WIDTH 480
 #define WIN_HEIGHT 480
+
+// Grid size
 #define SCALED_WIDTH 32
 #define SCALED_HEIGHT 32
 
 float scaleFactorX;
 float scaleFactorY;
 
-void mainLoop(ALLEGRO_EVENT_QUEUE * queue, ALLEGRO_TIMER * timer, ALLEGRO_DISPLAY * display);
+void mainLoop(ALLEGRO_EVENT_QUEUE * queue, ALLEGRO_TIMER * timer);
 void drawNode(node_t * node);
-node_t * reset(node_t * head, ALLEGRO_DISPLAY * display);
+node_t * reset(node_t * head);
 int genRandom(int max);
-void spawnApple(struct point * appleLocation, float scaleFactorX, float scaleFactorY, ALLEGRO_DISPLAY * display);
+void spawnApple(struct point * appleLocation);
 
 struct point headPos;
 node_t * head = NULL;
@@ -80,9 +83,9 @@ int main() {
    }
 
    // Spawn first apple
-   spawnApple(&applePos, scaleFactorX, scaleFactorY, display);
+   spawnApple(&applePos);
 
-   mainLoop(queue, timer, display);
+   mainLoop(queue, timer);
 
    // Free resources before exiting
    deleteSnake(head);
@@ -93,7 +96,7 @@ int main() {
    return 0;
 }
 
-void mainLoop(ALLEGRO_EVENT_QUEUE * queue, ALLEGRO_TIMER * timer, ALLEGRO_DISPLAY * display) {
+void mainLoop(ALLEGRO_EVENT_QUEUE * queue, ALLEGRO_TIMER * timer) {
    ALLEGRO_EVENT event;
 
    enum {up, right, down, left} direction = right;
@@ -109,8 +112,6 @@ void mainLoop(ALLEGRO_EVENT_QUEUE * queue, ALLEGRO_TIMER * timer, ALLEGRO_DISPLA
          case ALLEGRO_EVENT_TIMER:
             redraw = true;
             canPress = true;
-
-            // printf("[%d, %d]\n", headPos.x, headPos.y);
 
             switch (direction) {
                case up:
@@ -129,27 +130,30 @@ void mainLoop(ALLEGRO_EVENT_QUEUE * queue, ALLEGRO_TIMER * timer, ALLEGRO_DISPLA
 
             if (isColliding(head->next, head->p) // Check if the head has hit any part of the body
                   || headPos.x < scaleFactorX // Check if the head has hit the left wall
-                  || headPos.x >= al_get_display_width(display) - scaleFactorX // Check if the head has hit the right wall
+                  || headPos.x >= WIN_WIDTH - scaleFactorX // Check if the head has hit the right wall
                   || headPos.y < scaleFactorY // Check if the head has hit the top wall
-                  || headPos.y >= al_get_display_height(display) - scaleFactorY) { // Check if the head has hit the bottom wall
+                  || headPos.y >= WIN_HEIGHT - scaleFactorY) { // Check if the head has hit the bottom wall
                printf("Colidiu\n");
                direction = right;
-               head = reset(head, display);
+               head = reset(head);
 
+               // Spawn apple until it is not inside the snake
                do {
-                  spawnApple(&applePos, scaleFactorX, scaleFactorY, display);
+                  spawnApple(&applePos);
                } while (isColliding(head, applePos));
             }
 
-            head = createHead(head, headPos);
-
+            // If the snake has eaten the apple, spawn a new one and skip deleting the tail
             if (head->p.x == applePos.x && head->p.y == applePos.y) {
                do {
-                  spawnApple(&applePos, scaleFactorX, scaleFactorY, display);
+                  spawnApple(&applePos);
                } while (isColliding(head, applePos));
+            // If the snake has not eaten a apple, delete the tail
             } else {
                deleteTail(head);
             }
+
+            head = createHead(head, headPos);
 
             break;
          // User input
@@ -190,33 +194,34 @@ void mainLoop(ALLEGRO_EVENT_QUEUE * queue, ALLEGRO_TIMER * timer, ALLEGRO_DISPLA
       if (redraw && al_is_event_queue_empty(queue)) {
          al_clear_to_color(al_map_rgb(0, 0, 0));
 
-         // Draw the snake
-         forEach(head, *drawNode);
-
          // Draw the walls
          //    Horizontally
-         for (int i = 0; i <= al_get_display_width(display) - scaleFactorX; i += scaleFactorX) {
+         for (int i = 0; i <= WIN_WIDTH - scaleFactorX; i += scaleFactorX) {
             // Top row
             al_draw_filled_rectangle(i, 0, i + scaleFactorX, scaleFactorY, al_map_rgb(255, 255, 255));
             al_draw_rectangle(i, 0, i + scaleFactorX, scaleFactorY, al_map_rgb(0, 0, 0), 1);
 
             // Bottom row
-            al_draw_filled_rectangle(i, al_get_display_height(display) - scaleFactorY, i + scaleFactorX, al_get_display_height(display), al_map_rgb(255, 255, 255));
-            al_draw_rectangle(i, al_get_display_height(display) - scaleFactorY, i + scaleFactorX, al_get_display_height(display), al_map_rgb(0, 0, 0), 1);
+            al_draw_filled_rectangle(i, WIN_HEIGHT - scaleFactorY, i + scaleFactorX, WIN_HEIGHT, al_map_rgb(255, 255, 255));
+            al_draw_rectangle(i, WIN_HEIGHT - scaleFactorY, i + scaleFactorX, WIN_HEIGHT, al_map_rgb(0, 0, 0), 1);
          }
          //    Vertically
-         for (int i = scaleFactorY; i <= al_get_display_height(display) - scaleFactorY * 2; i += scaleFactorY) {
+         for (int i = scaleFactorY; i <= WIN_HEIGHT - scaleFactorY * 2; i += scaleFactorY) {
             // Left row
             al_draw_filled_rectangle(0, i, scaleFactorX, i + scaleFactorY, al_map_rgb(255, 255, 255));
             al_draw_rectangle(0, i, scaleFactorX, i + scaleFactorY, al_map_rgb(0, 0, 0), 1);
 
             // Right row
-            al_draw_filled_rectangle(al_get_display_width(display) - scaleFactorX, i, al_get_display_width(display), i + scaleFactorY, al_map_rgb(255, 255, 255));
-            al_draw_rectangle(al_get_display_width(display) - scaleFactorX, i, al_get_display_width(display), i + scaleFactorY, al_map_rgb(0, 0, 0), 1);
+            al_draw_filled_rectangle(WIN_WIDTH - scaleFactorX, i, WIN_WIDTH, i + scaleFactorY, al_map_rgb(255, 255, 255));
+            al_draw_rectangle(WIN_WIDTH - scaleFactorX, i, WIN_WIDTH, i + scaleFactorY, al_map_rgb(0, 0, 0), 1);
          }
 
+         // Draw the apple
          al_draw_filled_rectangle(applePos.x, applePos.y, applePos.x + scaleFactorX, applePos.y + scaleFactorY, al_map_rgb(255, 0, 0));
          al_draw_rectangle(applePos.x, applePos.y, applePos.x + scaleFactorX, applePos.y + scaleFactorY, al_map_rgb(0, 0, 0), 1);
+
+         // Draw the snake
+         forEach(head, *drawNode);
 
          al_flip_display();
          redraw = false;
@@ -229,12 +234,12 @@ void drawNode(node_t * node) {
    al_draw_rectangle(node->p.x, node->p.y, node->p.x + scaleFactorX, node->p.y + scaleFactorY, al_map_rgb(0, 0, 0), 1);
 }
 
-node_t * reset(node_t * head, ALLEGRO_DISPLAY * display) {
+node_t * reset(node_t * head) {
    deleteSnake(head);
 
    node_t * newhead = NULL;
-   headPos.x = ((al_get_display_width(display) / scaleFactorX) / 4) * scaleFactorX;
-   headPos.y = ((al_get_display_width(display) / scaleFactorY) / 2) * scaleFactorY;
+   headPos.x = ((WIN_WIDTH / scaleFactorX) / 4) * scaleFactorX;
+   headPos.y = ((WIN_HEIGHT / scaleFactorY) / 2) * scaleFactorY;
 
    for (int i = 0; i < 3; i++) {
       newhead = createHead(newhead, headPos);
@@ -255,9 +260,9 @@ int genRandom(int max) {
    return val;
 }
 
-void spawnApple(struct point * appleLocation, float scaleFactorX, float scaleFactorY, ALLEGRO_DISPLAY * display) {
-   int maxX = al_get_display_width(display) / scaleFactorX - 2;
-   int maxY = al_get_display_height(display) / scaleFactorY - 2;
+void spawnApple(struct point * appleLocation) {
+   int maxX = WIN_WIDTH / scaleFactorX - 2;
+   int maxY = WIN_HEIGHT / scaleFactorY - 2;
 
    appleLocation->x = genRandom(maxX) * scaleFactorX;
    appleLocation->y = genRandom(maxY) * scaleFactorY;
